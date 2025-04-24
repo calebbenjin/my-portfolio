8d96a53990b9f549f09e23c7d33172cd2247968c
calebbenjin <calebbenji0@gmail.com>
2025-04-24 11:11:23 +0100

    feat: remove pageloader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import IntroSection from "@/components/IntroSection";
 import ProjectSections from "@/components/ProjectSections";
 import UserProfile from "@/components/UserProfile";
 import WorkTogetherSection from "@/components/WorkTogetherSection";
-import PageLoader from "@/components/PageLoader";
 
 export default function Home() {
   return (
     <main className="mx-auto lg:w-6/12 w-11/12 pt-20">
-      <PageLoader />
       <UserProfile />
       <IntroSection />
       <ProjectSections />
